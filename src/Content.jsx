51beee2a22739771fec1f59b8cba1634ae25af10
Kimber Ogden <mx.kimber@gmail.com>
2024-09-09 22:57:02 -0500

    Create Signup, Login, and Logout components

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,14 @@
+import { Login } from "./Login";
+import { Logout } from "./Logout";
+import { Signup } from "./Signup";
+
 export function Content() {
   return (
     <div>
       <h1>Content here</h1>
+      <Signup />
+      <Login />
+      <Logout />
     </div>
   )
 }
---
+++ src/Logout.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export function Logout() {
+  const handleClick = (event) => {
+    event.preventDefault();
+    delete axios.defaults.headers.common["Authorization"];
+    localStorage.removeItem("jwt");
+    window.location.href = "/";
+  };
+
+  return (
+    <button onClick={handleClick}>
+      Logout
+    </button>
+  );
+}
