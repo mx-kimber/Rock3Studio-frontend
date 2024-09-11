95339d639fdc68148c66c5b3771054fadb45f68a
Kimber Ogden <mx.kimber@gmail.com>
2024-09-10 19:41:18 -0500

    conditionally render auth links in header

--- src/Content.jsx
+++ src/Content.jsx
@@ -5,20 +5,23 @@ import { UsersIndex } from "./UsersIndex";
 import { UserContext } from "./UserContext";
 import React, { useContext } from "react";
 
-
 export function Content() {
 
   const { currentUser } = useContext(UserContext);
 
+
   return (
     <div>
-     <div> <h1>
-        {currentUser ? `Welcome, ${currentUser.user_name}!` : 'Welcome, Guest!'}</h1>
+      
+      <div>
+        <h1> {currentUser ? `Welcome, ${currentUser.user_name}!` : 'Welcome, Guest!'} </h1>
       </div>
+      
       <Signup />
       <Login />
       <Logout />
       <UsersIndex />
+      
     </div>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,27 @@
+import { Logout } from "./Logout";
+
 export function Header() {
+  let authenticationLinks;
+  if (localStorage.jwt === undefined) {
+
+    authenticationLinks = (
+      <>
+        <button onClick={() => window.location.href = '/login'}>Login</button>  
+        <button onClick={() => window.location.href = '/signup'}>Signup</button>
+      </>
+    );
+  } else {
+
+    authenticationLinks = <Logout />;
+  }
+
   return (
     <header>
       <nav>
-        <a href="#">Home</a> | <a href="#">Link</a>
+        <button onClick={() => window.location.href = '/'}>Home</button>  
+        <button onClick={() => window.location.href = '/'}>Collection</button>  
+        {authenticationLinks}  
       </nav>
     </header>
-  )
+  );
 }
