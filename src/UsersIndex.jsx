ff18c1bba916a9b9e7bfca3756497fdd602e41af
Kimber Ogden <mx.kimber@gmail.com>
2024-09-10 19:32:44 -0500

    Create UserEdit component, handle modal functionality in UsersIndex to show UserEdit

--- src/UsersIndex.jsx
+++ src/UsersIndex.jsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { UserEdit } from './UserEdit';
+import { Modal } from "./Modal";
 
 export function UsersIndex() {
+
   const [users, setUsers] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
 
   useEffect(() => {
 
@@ -15,19 +20,33 @@ export function UsersIndex() {
       });
   }, []);
 
+  const handleCloseModal = () => {
+    setModalVisible(false);
+    setModalContent(null);
+  };
+
+  const handleUserEditModal = () => {
+    setModalVisible(true);
+    setModalContent(<UserEdit />);
+  };
+
   return (
     <div>
-      <h1>User Information</h1>
+      <h1>Users Index</h1>
  
         {users.map((user) => (
           <div key={user.id}>
             <p>Full Name: {user.full_name}</p>
             <p>User Name: {user.user_name}</p>
             <p>Email: {user.email}</p>
+            <button onClick={handleUserEditModal}>Update Information</button>
           </div>
         ))}
-   
+    <Modal show={modalVisible} onClose={handleCloseModal}>
+          {modalContent}
+        </Modal>
     </div>
+    
   );
 }
 
