287bbff2ccee204c2037013821dd9dd129c22eae
Kimber Ogden <mx.kimber@gmail.com>
2024-09-21 22:26:38 -0500

    Change sessions to local storage in logout

--- src/Logout.jsx
+++ src/Logout.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { UserContext } from "./UserContext";
 import axios from "axios";
 
@@ -8,9 +8,9 @@ export function Logout() {
   const handleClick = (event) => {
     event.preventDefault();
     delete axios.defaults.headers.common["Authorization"];
-    sessionStorage.removeItem("jwt");
-    setCurrentUser(null)
-    window.location.href = "/login"; 
+    localStorage.removeItem("jwt"); 
+    setCurrentUser(null);
+    window.location.href = "/login";
   };
 
   return (
